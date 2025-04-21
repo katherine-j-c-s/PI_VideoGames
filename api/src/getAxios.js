761f4b396ec76c0384d215944340835b6178c76a
katherine-j-c-s <69820647+katherine-j-c-s@users.noreply.github.com>
2025-04-21 13:23:12 -0300

    Update getAxios.js

--- api/src/getAxios.js
+++ api/src/getAxios.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 const https = require('https')
 require('dotenv').config()
 
-const API_KEY = 2c943d87ab6a45c5939d6a582f7aed98
-const domain = `https://api.rawg.io/api/games?key=${API_KEY}&dates=2019-09-01,2019-09-30&platforms=18,1,7`
+//const API_KEY = 2c943d87ab6a45c5939d6a582f7aed98
+const domain = `https://api.rawg.io/api/games?key=2c943d87ab6a45c5939d6a582f7aed98&dates=2019-09-01,2019-09-30&platforms=18,1,7`
 
 let instance
 
